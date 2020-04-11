d60e727c9a0d2fe4d4da451c4fe0f2e4e206123e
Prabhakar Bikkaneti <prabhakarb@nvidia.com>
2020-04-12 00:30:23 +0530

    Added Sample project for Fully-Connected DNN

---
+++ Sample Dense network/DenseNetwork.cuh
@@ -0,0 +1,14 @@
+
+#include "../DeepLearningInference/src/NN/NN.cuh"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <chrono> 
+
+
+std::unordered_map<std::string, std::vector<float>> parse_and_load_data(std::string fileName);
+
+
+std::vector<std::vector<std::vector<float>>> get_weights(std::vector<int>& layers_dims, std::unordered_map<std::string, std::vector<float>> weights_data);
